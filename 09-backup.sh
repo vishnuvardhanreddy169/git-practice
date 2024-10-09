8caeb052dc12d824d489985e487dcc33b6fbe864
vishnuvardhanreddy169 <vishnuvardhanreddy169@gmail.com>
2024-10-09 19:06:02 +0530

    shell

--- 09-backup.sh
+++ 09-backup.sh
@@ -3,6 +3,7 @@
 SOURCE_DIR=$1
 DEST_DIR=${2}
 DAYS=${3:-14} #if $3 is empty, default is 14 days.
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 
 R="\e[31m"
 G="\e[32m"
@@ -10,13 +11,14 @@ N="\e[0m"
 Y="\e[33m"
 
 USAGE(){
-    echo -e "$R USAGE:: $N sh 08-backup.sh <source> <destination> <days(optional)>"
+    echo -e "$R USAGE:: $N sh 19-backup.sh <source> <destination> <days(optional)>"
 }
 #check the source and destination are provided
 
 if [ $# -lt 2 ]
 then
     USAGE
+    exit 1
 fi
 
 if [ ! -d $SOURCE_DIR ]
@@ -29,13 +31,31 @@ then
     echo "$DEST_DIR does not exist...Please check"
 fi
 
-FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +$days)
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS)
 
-if [ -n $files ]
+echo "Files: $FILES"
+
+if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
 then
-    echo "files are found"
-else
-    echo "no older files than $days
-fi        
+    echo "Files are found"
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS | zip "$ZIP_FILE" -@
 
-   
+    #check if zip file is successfully created or not
+    if [ -f $ZIP_FILE ]
+    then
+        echo "Successfully zippped files older than $DAYS"
+        #remove the files after zipping
+        while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+        do
+            echo "Deleting file: $file"
+            rm -rf $file
+        done <<< $FILES
+    else
+        echo "Zipping the files is failed"
+        exit 1
+    fi
+else
+    echo "No files older than $DAYS"
+fi
+
