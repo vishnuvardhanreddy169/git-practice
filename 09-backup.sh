9131c88840f6804ab4f4fc2a2980510bb880881b
vishnuvardhanreddy169 <vishnuvardhanreddy169@gmail.com>
2024-10-09 18:38:19 +0530

    shell

--- 09-backup.sh
+++ 09-backup.sh
@@ -29,7 +29,7 @@ then
     echo "$DEST_DIR does not exist...Please check"
 fi
 
-FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +$days)
 
 if [ -n $files ]
 then
