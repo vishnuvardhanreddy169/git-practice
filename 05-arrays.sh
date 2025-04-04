cb9e129098494f6e00f2ee5a497d09cb04032272
vishnuvardhanreddy169 <vishnuvardhanreddy169@gmail.com>
2025-04-04 19:10:49 +0530

    05-array.sh

--- 05-arrays.sh
+++ 05-arrays.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 # index starts from 0, size is 3
-FRUITS=("APPLE" "KIWI" "ORANGE") #Array
+FRUITS=("APPLE" "KIWI" "ORANGE" "pineapple" "guava") #Array
 
 echo "First fruit is: ${FRUITS[0]}"
 echo "Second fruit is: ${FRUITS[1]}"
 echo "Third fruit is: ${FRUITS[2]}"
 
+echo "First fruit is: ${FRUITS[3]}"
+echo "Second fruit is: ${FRUITS[4]}"
+
+
+
+
 echo "First fruit is: ${FRUITS[@]}"
+
