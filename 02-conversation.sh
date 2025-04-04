901be9bd85e61530921ea2c528d38539637838de
vishnuvardhanreddy169 <vishnuvardhanreddy169@gmail.com>
2025-04-04 19:08:14 +0530

    02-converastion

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -4,5 +4,5 @@ person1=$1
 person2=$2
 
 echo "$person1:: Hi $person2, How are you?"
-echo "$person1:: Hello $person2. I am fine. How are you doing?"
+echo "$person2:: Hello $person1. I am fine. How are you doing?"
 
